002badeba27a543a61c3bb1991a8cf62127307b1
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2025-11-04 16:00:15 +0700

    add component button

--- react-hr-bo/src/components/atoms/Button/index.tsx
+++ react-hr-bo/src/components/atoms/Button/index.tsx
@@ -1,10 +1,11 @@
 import type React from 'react'
+import type { TVariants } from '../../../types'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string
   children?: React.ReactNode
   sizes?: 'sm' | 'md'
-  variant?: 'primary' | 'outlined' | 'secondary'
+  variant?: 'primary' | 'outlined' | 'secondary' | 'none'
   icon?: React.FC<React.SVGProps<SVGSVGElement>> | React.ReactNode
   onClick?: () => void
   disabled?: boolean
@@ -14,27 +15,27 @@ const Button: React.FC<ButtonProps> = ({
   className = '',
   type = 'button',
   variant = 'primary',
-  sizes = 'sm',
+  sizes='sm',
   children,
   icon: IconComponent,
   onClick,
   disabled = false,
   ...rest
 }) => {
-  
-  const SizesClass = {
+  const SizesClass: Record<'sm' | 'md', string> = {
     sm: 'text-sm px-4 py-3',
     md: 'text-sm py-3 px-7',
   }
 
-  const VariantsClass = {
+  const VariantsClass: Record<TVariants, string> = {
     primary: 'bg-brand-500 text-white hover:bg-brand-600 rounded-lg',
     outlined: 'border bg-white text-gray-500 border-gray-300 rounded-full',
     secondary: 'bg-gray-100 hover:bg-gray-200 rounded-lg',
+    none: '',
   }
   return (
     <button
-      className={`transition cursor-pointer ${VariantsClass[variant]} ${SizesClass[sizes]} ${className} ${disabled ? 'cursor-not-allowed opacity-50' : ''}`}
+      className={`${className} transition cursor-pointer ${VariantsClass[variant]} ${sizes ? SizesClass[sizes] : ''} ${disabled ? 'cursor-not-allowed opacity-50' : ''}`}
       type={type}
       onClick={onClick}
       {...rest}
