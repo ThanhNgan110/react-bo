24315f4c4d18ad29cf0690bc8adb14488f894112
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2025-11-04 16:11:00 +0700

    add type Option for data Select component

--- react-hr-bo/src/types/index.d.ts
+++ react-hr-bo/src/types/index.d.ts
@@ -1,5 +1,11 @@
+export type TVariants = 'primary' | 'outlined' | 'secondary' | 'none'
 export interface IMenuItem {
   name: string
   icon: React.ReactNode
   path: string
+}
+
+export type TOption = {
+  label: string
+  value: string
 }
