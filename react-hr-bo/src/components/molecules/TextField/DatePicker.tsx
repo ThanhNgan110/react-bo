132070b613982ab5aae28225086e01cdafb59d5d
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2025-11-27 15:15:55 +0700

    update: change parameter of dateSelected  for function

--- react-hr-bo/src/components/molecules/TextField/DatePicker.tsx
+++ react-hr-bo/src/components/molecules/TextField/DatePicker.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useRef } from 'react'
 import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.css'
+import type { FieldError } from 'react-hook-form'
 
 interface DatePickerProps {
   className?: string
   mode?: 'single' | 'multiple' | 'range' | 'time'
-  onChange?: flatpickr.Options.Hook | flatpickr.Options.Hook[]
+  onChange?: (dateSelected: string) => void
   placeholder?: string
   suffixIcon?: React.ReactNode
   defaultDate?: flatpickr.Options.DateOption
+  error?: FieldError | undefined
 }
 
 const DatePicker: React.FC<DatePickerProps> = ({
@@ -17,40 +19,45 @@ const DatePicker: React.FC<DatePickerProps> = ({
   placeholder,
   suffixIcon,
   defaultDate,
+  error,
 }) => {
   const inputRef = useRef<HTMLInputElement | null>(null)
 
   useEffect(() => {
-
     if (!inputRef.current) return
 
     const picker = flatpickr(inputRef.current, {
       mode,
-      dateFormat: 'Y-m-d',
+      dateFormat: 'd-m-Y',
       static: true,
       defaultDate,
       monthSelectorType: 'static',
-      onChange,
+      onChange: (_, dateStr) => {
+        onChange?.(dateStr)
+      },
     })
 
-    return () => {
-      picker.destroy()
-    }
+    return () => picker.destroy()
   }, [mode, onChange, defaultDate])
 
   return (
-    <div className="relative w-full">
-      <input
-        ref={inputRef}
-        placeholder={placeholder}
-        className="h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 bg-transparent text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-brand-500/20 dark:border-gray-700 dark:focus:border-brand-800"
-      />
-      {suffixIcon && (
-        <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-          {suffixIcon}
-        </span>
+    <>
+      <div className="relative w-full">
+        <input
+          ref={inputRef}
+          placeholder={placeholder}
+          className="h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400"
+        />
+        {suffixIcon && (
+          <span className="absolute -translate-y-1/2 right-3 top-1/2 pointer-events-none text-gray-500">
+            {suffixIcon}
+          </span>
+        )}
+      </div>
+      {error?.message && (
+        <p className="mt-1 text-sm text-error-500">{error.message}</p>
       )}
-    </div>
+    </>
   )
 }
 
